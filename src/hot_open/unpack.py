34a72580c31267c6ea6bbe1af07e734e775f9349
Samuel Northover-Naylor <50234141+samuelwnaylor@users.noreply.github.com>
2025-08-21 16:50:24 +0100

    Override parquet cache dir (#17)
    * support override of parquet cache directory
    
    * add reactive power mean column to scada df

--- src/hot_open/unpack.py
+++ src/hot_open/unpack.py
@@ -1,5 +1,7 @@
 """input data loading functions."""
 
+import logging
+import os
 from pathlib import Path
 
 import pandas as pd
@@ -8,11 +10,18 @@ from wind_up.caching import with_parquet_cache
 from .helpers import load_hot_10min_data, scada_df_to_wind_up_df
 from .paths import DATA_DIR, GLOBAL_CACHE_DIR
 
+logger = logging.getLogger(__name__)
+
 DATASET_START = pd.Timestamp("2016-01-01", tz="UTC")  # open source dataset start
 DATASET_END_EXCL = pd.Timestamp("2024-09-01", tz="UTC")  # open source dataset end
 
+parquet_cache_dir = Path(os.getenv("HOT_OPEN_CACHE_DIR", str(GLOBAL_CACHE_DIR)))
+logger.debug("Using parquet cache directory: %s", parquet_cache_dir)
+SCADA_DF_CACHE = parquet_cache_dir / "scada_df.parquet"
+METADATA_DF_CACHE = parquet_cache_dir / "metadata_df.parquet"
 
-@with_parquet_cache(GLOBAL_CACHE_DIR / "scada_df.parquet")
+
+@with_parquet_cache(SCADA_DF_CACHE)
 def unpack_local_scada_data(
     data_dir: Path = DATA_DIR,
     *,
@@ -20,6 +29,7 @@ def unpack_local_scada_data(
     end_dt_excl: pd.Timestamp = DATASET_END_EXCL,
 ) -> pd.DataFrame:
     """Unpack Hill of Towie open source SCADA data."""
+    logger.debug("Caching SCADA data to %s", SCADA_DF_CACHE)
     scada_df = load_hot_10min_data(
         data_dir=data_dir,
         wtg_numbers=list(range(1, 22)),
@@ -30,9 +40,10 @@ def unpack_local_scada_data(
     return scada_df_to_wind_up_df(scada_df, shutdown_duration_df=shutdown_duration_df)
 
 
-@with_parquet_cache(GLOBAL_CACHE_DIR / "metadata_df.parquet")
+@with_parquet_cache(METADATA_DF_CACHE)
 def unpack_local_meta_data(data_dir: Path = DATA_DIR) -> pd.DataFrame:
     """Unpack Hill of Towie open source turbine metadata."""
+    logger.debug("Caching metadata data to %s", METADATA_DF_CACHE)
     return (
         pd.read_csv(data_dir / "Hill_of_Towie_turbine_metadata.csv")
         .loc[:, ["Turbine Name", "Latitude", "Longitude"]]

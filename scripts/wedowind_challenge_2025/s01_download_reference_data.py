9b2c6b81fec91714a283ffd4d27692347789a207
Alex Clerc <alex.clerc@res-group.com>
2026-05-07 17:05:32 +0100

    Update s01_download_reference_data.py

--- scripts/wedowind_challenge_2025/s01_download_reference_data.py
+++ scripts/wedowind_challenge_2025/s01_download_reference_data.py
@@ -3,7 +3,7 @@
 import logging
 from pathlib import Path
 
-from hot_open.era5_helpers import HOT_LAT, HOT_LON, get_era5_hourly_df
+from hot_open.era5_helpers import HOT_LAT, HOT_LON, get_hot_era5_hourly_df
 from hot_open.settings import get_out_dir
 from scripts.logger import setup_logger
 
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     setup_logger(log_path)
     logger.info("log file is at %s", log_path)
 
-    hourly_dataframe = get_era5_hourly_df()
+    hourly_dataframe = get_hot_era5_hourly_df()
 
     fpath = out_dir / f"ERA5_{HOT_LAT:.2f}_{HOT_LON:.2f}.parquet"
     hourly_dataframe.to_parquet(fpath)

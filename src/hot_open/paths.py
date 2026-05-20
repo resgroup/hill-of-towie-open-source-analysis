c04416f5f059a0d93428590126e2073c38f44606
Alex Clerc <alex.clerc@res-group.com>
2026-05-20 14:55:53 +0100

    Delete paths.py

--- src/hot_open/paths.py
+++
@@ -1,6 +0,0 @@
-"""A module that contains the paths used in the analysis."""
-
-from hot_open.settings import get_wind_up_output_dir
-
-ANALYSES_DIR = get_wind_up_output_dir()
-DATA_DIR = ANALYSES_DIR / "zenodo_data"
